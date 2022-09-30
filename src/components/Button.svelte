625c8281ee01b7474568555f625c4fdbe1866ee1
Park Lu <park.lu@solartech.com.tw>
2022-09-30 14:21:56 +0800

    separate common components

---
+++ src/components/Button.svelte
@@ -0,0 +1,4 @@
+<button
+	class="px-4 py-2 border-2 border-cyan-700 hover:bg-cyan-700  text-cyan-700 hover:text-white">
+	<slot></slot>
+</button>
---
+++ src/components/TextList.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let name;
+	export let disabled = false;
+
+	export let text = '';
+</script>
+
+<textarea
+	id={name}
+	{name}
+	cols="30"
+	rows="10"
+	class="resize-none p-2 border-2 border-cyan-700"
+	{disabled}
+	bind:value={text} />
